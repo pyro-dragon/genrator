2fca7bd8e9008029a5675fc35155d088f637f65e
pyro-dragon <pyro@dragonscancode.com>
2019-01-26 21:25:22 +0000

    Working gender select form. Boostrap styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BodyFormComponent } from './body-form/body-form.component';
@@ -10,7 +11,8 @@ import { BodyFormComponent } from './body-form/body-form.component';
     BodyFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/body-form/body-form.component.ts
+++ src/app/body-form/body-form.component.ts
@@ -1,29 +1,28 @@
-import { Component, Input } from '@angular/core';
-import { NgModel } from "@angular/forms";
+import { Component, Input } from "@angular/core";
 
 @Component({
-	selector: 'app-body-form',
-	templateUrl: './body-form.component.html',
-	styleUrls: ['./body-form.component.css']
+	selector: "app-body-form",
+	templateUrl: "./body-form.component.html",
+	styleUrls: ["./body-form.component.css"]
 })
 export class BodyFormComponent {
 
-	options = {};
+	options;
 
-    maleChoices = {
-        base: [{dn: "Default (male)", fn: "male.png"}], 
-        coat: [{dn: "Spotted", fn: "spotted.png"}, {dn: "Striped", fn: "striped.png"}], 
-        leftEar: [{dn: "Rounded", fn: "rounded.png"}, {dn: "Pointed", fn: "pointed.png"}, {dn: "Damaged", fn: "damaged.png"}], 
-        rightEar: [{dn: "Rounded", fn: "rounded.png"}, {dn: "Pointed", fn: "pointed.png"}, {dn: "Damaged", fn: "damaged.png"}], 
-        hair: [{dn: "Spiked", fn: "spiked.png"}, {dn: "Buzz Cut", fn: "buzzed.png"}]
-    };
+	maleChoices = {
+		base: [{dn: "Default (male)", fn: "male.png"}], 
+		coat: [{dn: "Spotted", fn: "spotted.png"}, {dn: "Striped", fn: "striped.png"}], 
+		leftEar: [{dn: "Rounded", fn: "rounded.png"}, {dn: "Pointed", fn: "pointed.png"}, {dn: "Damaged", fn: "damaged.png"}], 
+		rightEar: [{dn: "Rounded", fn: "rounded.png"}, {dn: "Pointed", fn: "pointed.png"}, {dn: "Damaged", fn: "damaged.png"}], 
+		hair: [{dn: "Spiked", fn: "spiked.png"}, {dn: "Buzz Cut", fn: "buzzed.png"}]
+	};
 
-    femaleChoices = {
-        base: [{dn: "Default (female)", fn: "female.png"}], 
-        coat: [{dn: "Spotted", fn: "spotted.png"}, {dn: "Striped", fn: "striped.png"}], 
-        leftEar: [{dn: "Rounded", fn: "rounded.png"}, {dn: "Pointed", fn: "pointed.png"}, {dn: "Damaged", fn: "damaged.png"}], 
-        rightEar: [{dn: "Rounded", fn: "rounded.png"}, {dn: "Pointed", fn: "pointed.png"}, {dn: "Damaged", fn: "damaged.png"}], 
-        hair: [{dn: "Spiked", fn: "spiked.png"}, {dn: "Flowing", fn: "flowing.png"}]
+	femaleChoices = {
+		base: [{dn: "Default (female)", fn: "female.png"}], 
+		coat: [{dn: "Spotted", fn: "spotted.png"}, {dn: "Striped", fn: "striped.png"}], 
+		leftEar: [{dn: "Rounded", fn: "rounded.png"}, {dn: "Pointed", fn: "pointed.png"}, {dn: "Damaged", fn: "damaged.png"}], 
+		rightEar: [{dn: "Rounded", fn: "rounded.png"}, {dn: "Pointed", fn: "pointed.png"}, {dn: "Damaged", fn: "damaged.png"}], 
+		hair: [{dn: "Spiked", fn: "spiked.png"}, {dn: "Flowing", fn: "flowing.png"}]
 	};
 
 	bodyChoices = [
@@ -35,11 +34,19 @@ export class BodyFormComponent {
 			displayName: "Female", 
 			choices: this.femaleChoices
 		}
-	]
+	];
 	
 	bodyMap;
+	JSON;
 
 	@Input() body: {};
 	
-	constructor() {}
+	constructor() {
+
+		this.JSON = JSON;
+	}
+
+	public changeOption(event: any): void {
+		console.log(event);
+	}
 }

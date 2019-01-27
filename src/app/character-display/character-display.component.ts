a5636e5fe8c1636069e1ea49948be4a052a692e6
pyro-dragon <pyro@dragonscancode.com>
2019-01-27 23:30:30 +0000

    Added images. Made images render on the canvas.

--- src/app/character-display/character-display.component.ts
+++ src/app/character-display/character-display.component.ts
@@ -13,17 +13,17 @@ export class CharacterDisplayComponent implements AfterViewInit {
 
 	constructor() {}
 
-	ngAfterViewInit(){
-		if(this.canvas1.nativeElement.getContext){
+	ngAfterViewInit() {
+		if (this.canvas1.nativeElement.getContext) {
 
 			let context = this.canvas1.nativeElement.getContext("2d");
 			let layer1 = new Image();
 			let layer2 = new Image();
-			layer1.src = "/assets/layer1a.png";
-			layer1.onload = function(){
+			layer1.src = "/assets/Bodies/Female/Body/Base.png";
+			layer1.onload = function() {
 
-				layer2.src = "/assets/layer2.png";
-				layer2.onload = function(){
+				layer2.src = "/assets/Bodies/Female/Body/Lines.png";
+				layer2.onload = function() {
 
 					context.drawImage(layer1, 0, 0);
 					context.globalCompositeOperation = "source-in";
@@ -31,23 +31,23 @@ export class CharacterDisplayComponent implements AfterViewInit {
 					context.fillRect(0, 0 , layer1.width, layer1.height);
 					context.globalCompositeOperation = "source-over";
 					context.drawImage(layer2, 0, 0);
-				}
-			}
+				};
+			};
 		}
 		else {
 			return;
 		}
 
-		if(this.canvas2.nativeElement.getContext){
+		if (this.canvas2.nativeElement.getContext) {
 
 			let context = this.canvas2.nativeElement.getContext("2d");
 			let layer3 = new Image();
 			let layer4 = new Image();
-			layer3.src = "/assets/layer3.png";
-			layer3.onload = function(){
+			layer3.src = "/assets/Bodies/Female/Hair/Ironed Base.png";
+			layer3.onload = function() {
 
-				layer4.src = "/assets/layer4.png";
-				layer4.onload = function(){
+				layer4.src = "/assets/Bodies/Female/Hair/Ironed Lines.png";
+				layer4.onload = function() {
 
 					context.drawImage(layer3, 0, 0);
 					context.globalCompositeOperation = "source-in";
@@ -55,9 +55,9 @@ export class CharacterDisplayComponent implements AfterViewInit {
 					context.fillRect(0, 0 , layer3.width, layer3.height);
 					context.globalCompositeOperation = "source-over";
 					context.drawImage(layer4, 0, 0);
-				}
+				};
 				
-			}
+			};
 		}
 		else {
 			return;
